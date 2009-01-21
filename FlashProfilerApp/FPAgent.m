7967a18b5de8ec946deca7dae1103cd3bf9a4569
peter royal <peter.royal@pobox.com>
2009-01-20 19:50:41 -0800

    copyright me

--- FlashProfilerApp/FPAgent.m
+++ FlashProfilerApp/FPAgent.m
@@ -3,7 +3,7 @@
 //  FlashProfilerApp
 //
 //  Created by peter royal on 1/8/09.
-//  Copyright 2009 Electrotank, Inc.. All rights reserved.
+//  Copyright 2009 Peter Royal. All rights reserved.
 //
 
 #import "FPAgent.h"
--- FlashProfilerApp/FPMemoryUsage.m
+++ FlashProfilerApp/FPMemoryUsage.m
@@ -3,7 +3,7 @@
 //  FlashProfilerApp
 //
 //  Created by peter royal on 1/7/09.
-//  Copyright 2009 Electrotank, Inc.. All rights reserved.
+//  Copyright 2009 Peter Royal. All rights reserved.
 //
 
 #import "FPMemoryUsage.h"
